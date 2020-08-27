891a7dc5924cc397807a71a5de3f852da1d9fb07
cjreside <connorjreside@gmail.com>
2020-08-27 10:16:51 -0400

    update

--- query.sql
+++ query.sql
@@ -6,37 +6,34 @@ CREATE TABLE income (
 
 CREATE TABLE sales (
   metro_area TEXT PRIMARY KEY,
-  Median_Home_Price_1_18 INT, 	
-  Median_Home_Price_2_18 INT,	
-  Median_Home_Price_3_18 INT,	
-  Median_Home_Price_4_18 INT,	
-  Median_Home_Price_5_18 INT,	
-  Median_Home_Price_6_18 INT,	
-  Median_Home_Price_7_18 INT,	
-  Median_Home_Price_8_18 INT,	
-  Median_Home_Price_9_18 INT,	
-  Median_Home_Price_10_18 INT,	
-  Median_Home_Price_11_18 INT,	
-  Median_Home_Price_12_18 INT,	
-  Sales_Average_2018 INT
+  median_home_price_1_18 INT, 	
+  median_home_price_2_18 INT,	
+  median_home_price_3_18 INT,	
+  median_home_price_4_18 INT,	
+  median_home_price_5_18 INT,	
+  median_home_price_6_18 INT,	
+  median_home_price_7_18 INT,	
+  median_home_price_8_18 INT,	
+  median_home_price_9_18 INT,	
+  median_home_price_10_18 INT,	
+  median_home_price_11_18 INT,	
+  median_home_price_12_18 INT,	
+  sales_average_2018 INT
 );
 
 CREATE TABLE rent (
   metro_area TEXT PRIMARY KEY,
-  Median_Rent_Price_1_18 INT, 	
-  Median_Rent_Price_2_18 INT,	
-  Median_Rent_Price_3_18 INT,	
-  Median_Rent_Price_4_18 INT,	
-  Median_Rent_Price_5_18 INT,	
-  Median_Rent_Price_6_18 INT,	
-  Median_Rent_Price_7_18 INT,	
-  Median_Rent_Price_8_18 INT,	
-  Median_Rent_Price_9_18 INT,	
-  Median_Rent_Price_10_18 INT,	
-  Median_Rent_Price_11_18 INT,	
-  Median_Rent_Price_12_18 INT,	
-  Rent_Average_2018 INT
+  median_rent_price_1_18 INT, 	
+  median_rent_price_2_18 INT,	
+  median_rent_price_3_18 INT,	
+  median_rent_price_4_18 INT,	
+  median_rent_price_5_18 INT,	
+  median_rent_price_6_18 INT,	
+  median_rent_price_7_18 INT,	
+  median_rent_price_8_18 INT,	
+  median_rent_price_9_18 INT,	
+  median_rent_price_10_18 INT,	
+  median_rent_price_11_18 INT,	
+  median_rent_price_12_18 INT,	
+  rent_average_2018 INT
 );
-
-CREATE USER local_user with PASSWORD 'password123';
-
